4c613a9d38aac54774187cc488968133feab0d73
Sean Hurley <sean@teztechnology.com>
2024-06-07 13:29:35 -0500

    Fix search params missing

---
+++ src/app/(main)/authenticate/Authenticate.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useStytch, useStytchUser } from "@stytch/nextjs";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const OAUTH_TOKEN = "oauth";
+const MAGIC_LINKS_TOKEN = "magic_links";
+
+/**
+ * During both the Magic link and OAuth flows, Stytch will redirect the user back to your application to a specified redirect URL (see Login.tsx).
+ * Stytch will append query parameters to the redirect URL which are then used to complete the authentication flow.
+ * A redirect URL for this example app will look something like: http://localhost:3000/authenticate?stytch_token_type=magic_links&token=abc123
+ *
+ * The AuthenticatePage will detect the presence of a token in the query parameters, and attempt to authenticate it.
+ *
+ * On successful authentication, a session will be created and the user will be redirect to /profile.
+ */
+const Authenticate = () => {
+  const { user, isInitialized } = useStytchUser();
+  const searchParams = useSearchParams();
+  const stytch = useStytch();
+  const router = useRouter();
+
+  const token = searchParams.get("token");
+  const stytch_token_type = searchParams.get("stytch_token_type");
+
+  console.log(`${token}::${stytch_token_type}`);
+
+  useEffect(() => {
+    if (stytch && !user && isInitialized) {
+      if (token && stytch_token_type === OAUTH_TOKEN) {
+        stytch.oauth.authenticate(token, {
+          session_duration_minutes: 60,
+        });
+      } else if (token && stytch_token_type === MAGIC_LINKS_TOKEN) {
+        stytch.magicLinks.authenticate(token, {
+          session_duration_minutes: 60,
+        });
+      } else console.log(`STYTCH_TOKEN_TYPE: ${stytch_token_type}`);
+    }
+  }, [isInitialized, router, stytch, user]);
+
+  useEffect(() => {
+    if (!isInitialized) {
+      return;
+    }
+
+    if (user) {
+      router.replace("/location");
+    }
+  }, [router, user, isInitialized]);
+
+  return null;
+};
+
+export default Authenticate;
--- src/app/(main)/authenticate/page.tsx
+++ src/app/(main)/authenticate/page.tsx
@@ -1,61 +1,12 @@
-"use client";
+import { Suspense } from "react";
+import Authenticate from "./Authenticate";
 
-import { useStytch, useStytchUser } from "@stytch/nextjs";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-const OAUTH_TOKEN = "oauth";
-const MAGIC_LINKS_TOKEN = "magic_links";
-
-/**
- * During both the Magic link and OAuth flows, Stytch will redirect the user back to your application to a specified redirect URL (see Login.tsx).
- * Stytch will append query parameters to the redirect URL which are then used to complete the authentication flow.
- * A redirect URL for this example app will look something like: http://localhost:3000/authenticate?stytch_token_type=magic_links&token=abc123
- *
- * The AuthenticatePage will detect the presence of a token in the query parameters, and attempt to authenticate it.
- *
- * On successful authentication, a session will be created and the user will be redirect to /profile.
- */
-const Authenticate = ({
-  params,
-  searchParams,
-}: {
-  params: any;
-  searchParams: any;
-}) => {
-  const { user, isInitialized } = useStytchUser();
-  const stytch = useStytch();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (stytch && !user && isInitialized) {
-      const token = searchParams.token;
-      const stytch_token_type = searchParams.stytch_token_type;
-
-      console.log(`SEARCH_PARAMS:: ${JSON.stringify(searchParams)}`);
-
-      if (token && stytch_token_type === OAUTH_TOKEN) {
-        stytch.oauth.authenticate(token, {
-          session_duration_minutes: 60,
-        });
-      } else if (token && stytch_token_type === MAGIC_LINKS_TOKEN) {
-        stytch.magicLinks.authenticate(token, {
-          session_duration_minutes: 60,
-        });
-      } else console.log(`STYTCH_TOKEN_TYPE: ${stytch_token_type}`);
-    }
-  }, [isInitialized, router, searchParams, stytch, user]);
-
-  useEffect(() => {
-    if (!isInitialized) {
-      return;
-    }
-    if (user) {
-      router.replace("/location");
-    }
-  }, [router, user, isInitialized]);
-
-  return null;
+const AuthenticatePage = async () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Authenticate />
+    </Suspense>
+  );
 };
 
-export default Authenticate;
+export default AuthenticatePage;
